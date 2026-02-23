31b01996855eef2718d99f3a855f3ee457632d53
xuzeyu <286513187@qq.com>
2026-02-23 16:02:30 +0800

    feat: 添加对 Inter 和 JetBrains Mono 字体的支持，更新相关依赖

--- index.tsx
+++ index.tsx
@@ -1,3 +1,11 @@
+import '@fontsource/inter/latin-300.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-600.css';
+import '@fontsource/inter/latin-700.css';
+import '@fontsource/jetbrains-mono/latin-400.css';
+import '@fontsource/jetbrains-mono/latin-500.css';
+import '@fontsource/jetbrains-mono/latin-700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
